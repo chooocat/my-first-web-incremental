62ca11ad52332648f4f6a147ab109a4a8323a38b
chooocat <120656085+chooocat@users.noreply.github.com>
2024-01-02 19:51:36 +0800

    Now visibility changed.

--- script.js
+++ script.js
@@ -24,6 +24,14 @@ function saveData() {
     localStorage.setItem("clicks", totalClicks.toString())
 }
 
+function InitUps() {
+    for (i = 1; i <= upgradeAmt; i++) {
+        if (i != 1) {
+            document.getElementById("Upgrade"+i).style.visibility = "hidden"
+        } else {continue}
+    }
+}
+
 window.onload = function() {
     loadData()
     Loop()
@@ -173,6 +181,7 @@ function upgrade(upAmo, price, type, boost) {
         document.getElementById("upDis"+upAmo).innerHTML = "Bought!"
         document.getElementById("ClickDisplay").innerHTML = Format(clicks)
         console.log("BOUGHT")
+        document.getElementById("Upgrade"+upAmo+1).style.visibility = "visible"
 
         if (type == 1) {
             clickGain = clickGain.times(boost)
@@ -183,6 +192,7 @@ function upgrade(upAmo, price, type, boost) {
             document.getElementById("upDis"+upAmo).innerHTML = "Bought!"
             document.getElementById("ClickDisplay").innerHTML = Format(clicks)
             console.log("BOUGHT")
+            document.getElementById("Upgrade"+upAmo+1).style.visibility = "visible"
             decUp["up"+upAmo] = true
         }
     }
