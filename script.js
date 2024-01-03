76feb2daa1adeeb846b046671c5204aa08bd2d6b
chooocat <120656085+chooocat@users.noreply.github.com>
2024-01-03 15:00:54 +0800

    New update

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-upgradeAmt = 28
+upgradeAmt = 44
+decAmo = 14-1
 clicks = new Decimal (0)
 totalClicks = new Decimal (0)
 clickGain = new Decimal (1)
+clickPow = new Decimal (1)
 decimalClickBoost = new Decimal (1)
+superClicks = new Decimal (0)
+superClickBoost = new Decimal (1)
 decUp = {
     up7: false,
     up9: false,
@@ -12,19 +16,28 @@ decUp = {
     up19: false,
     up22: false,
     up25: false,
+    up32: false,
+    up33: false,
+    up35: false,
+    up38: false,
+    up40: false,
+    up44: false,
 }
 
 function loadData() {
     clicks = new Decimal(localStorage.getItem("clicks")) || new Decimal (0)
     totalClicks = clicks
+    superClicks = new Decimal(localStorage.getItem("sc")) || new Decimal (0)
     buyMax()
 }
 
 function saveData() {
     localStorage.setItem("clicks", totalClicks.toString())
+    localStorage.setItem("sc", superClicks.toString())
 }
 
 function InitUps() {
+    document.getElementById("SupCl").style.visibility = "collapse"
     for (i = 2; i <= upgradeAmt; i++) {
         document.getElementById("Upgrade"+i).style.visibility = "collapse"
     }
@@ -139,8 +152,9 @@ function GenerateClicks() {
                     a = a + 0.002
                 }
             }
-            base = base + a
+
             document.getElementById("upDis19").innerHTML = "Currently: +"+(Math.floor(a*1000))/1000+" base"
+            base = base + a
         }
         let Formula = clicks.pow(base).add(1)
         boostCombC = Formula
@@ -153,19 +167,53 @@ function GenerateClicks() {
         if (decUp.up25 == true) {
             base = base + 0.05
         }
+        if (decUp.up33 == true) {
+            base = base + 0.05
+        }
+        if (decUp.up38 == true) {
+            let a = 0
+            for (i = 1; i <= upgradeAmt; i++) {
+                if (document.getElementById("upDis"+i).innerHTML == "Bought!") {
+                    a = a + 0.001
+                }
+            }
+
+            document.getElementById("upDis38").innerHTML = "Currently: +"+(Math.floor(a*1000))/1000+" base"
+            base = base + a
+        }
         let Formula = clicks.pow(base).add(1)
         boostCombC2 = Formula
         document.getElementById("upDis22").innerHTML = "Currently: "+Format(Formula)+"x Points"
         base = 0.05
     }
 
+    if (decUp.up32 == true) {
+        document.getElementById("SupCl").style.visibility = "visible"
+        superClicks = clicks.log(10)
+        let two = new Decimal("1.15")
+        if (decUp.up35 == true) {
+            two = two.add(0.05)
+        }
+        if (decUp.up40 == true) {
+            two = two.add(0.05)
+        }
+        if (decUp.up44 == true) {
+            two = two.add(0.1)
+        }
+        superClickBoost = two.pow(superClicks)
+        two.mag = 1.15
+    }
+
     decimalClickBoost = boostCombC.times(boostCombC2)
 
-    let gain = clickGain.mul(decimalClickBoost)
+    let gain = (clickGain.mul(decimalClickBoost).mul(superClickBoost)).pow(clickPow)
     clicks = clicks.add(gain)
     totalClicks = totalClicks.add(gain)
     clickDisplay = Format(clicks)
+    sclickDisplay = Format(superClicks)
     document.getElementById("ClickDisplay").innerHTML = clickDisplay
+    document.getElementById("ClickDisplay2").innerHTML = sclickDisplay
+    document.getElementById("SCAB").innerHTML = Format(superClickBoost)+"x"
 }
 
 function Loop() {
@@ -186,6 +234,8 @@ function upgrade(upAmo, price, type, boost) {
 
         if (type == 1) {
             clickGain = clickGain.times(boost)
+        } else if (type == 2) {
+            clickPow = clickPow.add(boost)
         }
     } else if (type == 0) {
         if (clicks.gte(price) && decUp["up"+upAmo] == false) {
@@ -197,6 +247,28 @@ function upgrade(upAmo, price, type, boost) {
                 document.getElementById("Upgrade"+(upAmo+1)).style.visibility = "visible"
             }
             decUp["up"+upAmo] = true
+        }
+    }
+}
+
+function resetData() {
+    let conf = confirm("Are you sure you want to reset? You can't undo this")
+    if (conf == true) {
+        console.log("RESET")
+        let conf2 = confirm("ARE YOU REALLY SURE?????")
+        if (conf2 == true) {
+            for (let i = 0; i <= decAmo; i++) {
+                decUp[i] = false
+            }
+            clickGain = 1
+            clickPow = 1
+            decimalClickBoost = 1
+            superClicks = 0
+            superClickBoost = 1
+            clicks = 0
+            totalClicks = 0
+            saveData()
+            window.close()
         }
     }
 }
