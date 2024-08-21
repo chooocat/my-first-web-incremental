74511452e74883eec9bea6bea2c9f6d0401f0991
chooocat <120656085+chooocat@users.noreply.github.com>
2024-08-21 18:37:09 +0800

    Final Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-upgradeAmt = 44
-decAmo = 14-1
+upgradeAmt = 58
+decAmo = 23-1
 clicks = new Decimal (0)
 totalClicks = new Decimal (0)
 clickGain = new Decimal (1)
@@ -7,7 +7,9 @@ clickPow = new Decimal (1)
 decimalClickBoost = new Decimal (1)
 superClicks = new Decimal (0)
 superClickBoost = new Decimal (1)
-totalCPS = new Decimal(1)
+megaClicks = new Decimal (0)
+megaClickBoost = new Decimal (1)
+totalCPS = new Decimal (1)
 decUp = {
     up7: false,
     up9: false,
@@ -22,23 +24,35 @@ decUp = {
     up35: false,
     up38: false,
     up40: false,
+    up42: false,
     up44: false,
+    up45: false,
+    up46: false,
+    up49: false,
+    up51: false,
+    up54: false,
+    up55: false,
+    up56: false,
+    up57: false,
 }
 
 function loadData() {
     clicks = new Decimal(localStorage.getItem("clicks")) || new Decimal (0)
     totalClicks = clicks
-    superClicks = new Decimal(localStorage.getItem("sc")) || new Decimal (0)
+    //superClicks = new Decimal(localStorage.getItem("sc")) || new Decimal (0)
+    megaClicks = new Decimal(localStorage.getItem("mc")) || new Decimal (0)
     buyMax()
 }
 
 function saveData() {
     localStorage.setItem("clicks", totalClicks.toString())
-    localStorage.setItem("sc", superClicks.toString())
+    //localStorage.setItem("sc", superClicks.toString())
+    localStorage.setItem("mc", superClicks.toString())
 }
 
 function InitUps() {
     document.getElementById("SupCl").style.visibility = "collapse"
+    document.getElementById("MegCl").style.visibility = "collapse"
     for (i = 2; i <= upgradeAmt; i++) {
         document.getElementById("Upgrade"+i).style.visibility = "collapse"
     }
@@ -131,6 +145,7 @@ function Format(Val) {
 function GenerateClicks() {
     boostCombC = new Decimal (1)
     boostCombC2 = new Decimal (1)
+    boostCombC3 = new Decimal (1)
 
     if (decUp.up7 == true) {
         let base = 0.1
@@ -157,6 +172,9 @@ function GenerateClicks() {
             document.getElementById("upDis19").innerHTML = "Currently: +"+(Math.floor(a*1000))/1000+" base"
             base = base + a
         }
+        if (decUp.up46 == true) {
+            base = base+0.01
+        }
         let Formula = clicks.pow(base).add(1)
         boostCombC = Formula
         document.getElementById("upDis7").innerHTML = "Currently: "+Format(Formula)+"x Points"
@@ -182,41 +200,94 @@ function GenerateClicks() {
             document.getElementById("upDis38").innerHTML = "Currently: +"+(Math.floor(a*1000))/1000+" base"
             base = base + a
         }
+        if (decUp.up46 == true) {
+            base = base+0.01
+        }
         let Formula = clicks.pow(base).add(1)
         boostCombC2 = Formula
         document.getElementById("upDis22").innerHTML = "Currently: "+Format(Formula)+"x Points"
         base = 0.05
     }
 
+    if (decUp.up51 == true) {
+        let base = 10
+        let Formula = clicks.log(base).add(1)
+        boostCombC3 = Formula
+        document.getElementById("upDis51").innerHTML = "Currently: "+Format(Formula)+"x Points"
+        base = 10
+    }
+
     if (decUp.up32 == true) {
         document.getElementById("SupCl").style.visibility = "visible"
-        superClicks = clicks.log(10)
         let two = new Decimal("1.15")
+        let logbase = 10
         if (decUp.up35 == true) {
             two = two.add(0.05)
         }
         if (decUp.up40 == true) {
             two = two.add(0.05)
         }
+        if (decUp.up42 == true) {
+            two = two.add(0.04)
+        }
         if (decUp.up44 == true) {
             two = two.add(0.1)
         }
+        if (decUp.up45 == true) {
+            two = two.add(0.09)
+        }
+        if (decUp.up49 == true) {
+            two = two.add(0.08)
+        }
+        if (decUp.up54 == true) {
+            two = two.add(0.01)
+            logbase = logbase - 1
+        }
+        if (decUp.up55 == true) {
+            two = two.add(0.01)
+            logbase = logbase - 1
+        }
+        if (decUp.up56 == true) {
+            two = two.add(0.01)
+            logbase = logbase - 1
+        }
+        if (decUp.up56 == true) {
+            two = two.add(0.01)
+            logbase = logbase - 1
+        }
+        if (decUp.up57 == true) {
+            two = two.add(megaClickBoost)
+        }
+        superClicks = clicks.log(logbase)
         superClickBoost = two.pow(superClicks)
         two.mag = 1.15
+        logbase = 10
     }
 
-    decimalClickBoost = boostCombC.times(boostCombC2)
+    if (decUp.up57 == true) {
+        document.getElementById("MegCl").style.visibility = "visible"
+        //let two = new Decimal("1.15")
+        let logbase = 5
+        megaClicks = superClicks.log(logbase)
+        megaClickBoost = megaClicks.div(900).toNumber()
+        logbase = 10
+    }
+
+    decimalClickBoost = boostCombC.times(boostCombC2).times(boostCombC3)
 
     let gain = (clickGain.mul(decimalClickBoost).mul(superClickBoost)).pow(clickPow)
     clicks = clicks.add(gain)
     totalClicks = totalClicks.add(gain)
     clickDisplay = Format(clicks)
     sclickDisplay = Format(superClicks)
+    mclickDisplay = Format(megaClicks)
     totalCPS = gain.mul(10)
     document.getElementById("clPSec").innerHTML = Format(totalCPS)
     document.getElementById("ClickDisplay").innerHTML = clickDisplay
     document.getElementById("ClickDisplay2").innerHTML = sclickDisplay
+    document.getElementById("ClickDisplay3").innerHTML = mclickDisplay
     document.getElementById("SCAB").innerHTML = Format(superClickBoost)+"x"
+    document.getElementById("MCAB").innerHTML = "+"+Math.floor(megaClickBoost*10000)/10000
 }
 
 function Loop() {
@@ -255,7 +326,7 @@ function upgrade(upAmo, price, type, boost) {
 }
 
 function resetData() {
-    let conf = confirm("Are you sure you want to reset? You can't undo this")
+    /*let conf = confirm("Are you sure you want to reset? You can't undo this")
     if (conf == true) {
         console.log("RESET")
         let conf2 = confirm("ARE YOU REALLY SURE?????")
@@ -273,5 +344,8 @@ function resetData() {
             saveData()
             window.close()
         }
-    }
+    }*/
+    localStorage.clear()
+    location.reload()
+    console.log()
 }
